8e43504debe684ca1e2f9685fc74cab9d34ddf1a
Deep6234 <129764385+Deep6234@users.noreply.github.com>
2024-10-11 14:41:13 +0530

    Update full_adder.vhd

--- combinational_circuits/full_adder/full_adder.vhd
+++ combinational_circuits/full_adder/full_adder.vhd
@@ -1,1 +1,19 @@
+-- File: full_adder.vhd
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity full_adder is
+    Port ( A : in STD_LOGIC;
+           B : in STD_LOGIC;
+           Cin : in STD_LOGIC;
+           Sum : out STD_LOGIC;
+           Cout : out STD_LOGIC);
+end full_adder;
+
+architecture Behavioral of full_adder is
+begin
+    -- Logic for Full Adder
+    Sum <= (A XOR B) XOR Cin;
+    Cout <= (A AND B) OR (Cin AND (A XOR B));
+end Behavioral;
 
